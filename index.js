a436fd994d5f8954659c462b6dafcde4296ed37c
Pawan Rajora <164163556+pawanrajora143@users.noreply.github.com>
2024-06-24 20:28:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const btns = document.querySelectorAll(".btn")
 const resertBtn = document.querySelector("#resert-btn")
 const msgContainer = document.querySelector(".msg-container")
+const msgContainer2 = document.querySelector(".tieMatch")
 const newGameBtn = document.querySelector("#new-btn")
+const newGameBtn2 = document.querySelector("#new-btn2")
 const msg = document.querySelector("#msg")
+const tieMatch = document.querySelector(".tieMatch")
 
 let turn0 = true ; // playerX , player0
 // let count = 0;
@@ -22,7 +25,11 @@ const resetGame = () =>{
     turn0 = true;
     enableBtns();
     msgContainer.classList.add("hide")
+    msgContainer2.classList.add("hide")
 }
+
+let matchTie = true;
+let checkTie = 0
 
 btns.forEach((btns)=>{
 
@@ -30,22 +37,38 @@ btns.forEach((btns)=>{
         console.log("box was clicked");
         if (turn0) {
             //player0
-            btns.innerText = "0";
+            btns.innerHTML = "<img src=\'circle.png\' width=\'50px\' height=\'50px\' class='img1'>"
             turn0 = false;
+            
         }
         else{
             //playery
-            btns.innerText = "X";
+            btns.innerHTML = "<img src=\'cross.png\' width=\'50px\' height=\'50px\' class='img1'>"
             turn0 = true;
         }
         btns.disabled = true;
         checkWinner();
+
+        if(btns.disabled === true){
+            checkTie += 1
+            console.log(checkTie)
+            console.log(matchTie)
+
+            if(checkTie == 9){
+                console.log(matchTie)
+                matchTieShow()
+
+            }
+        }
+
+
     });
 });
 
 const disableBtns = () => {
     for(let stopbtn of btns ){
         stopbtn.disabled = true;
+    
     }
 }
 
@@ -53,19 +76,22 @@ const enableBtns = () => {
     for(let stopbtn of btns ){
         stopbtn.disabled = false;
         stopbtn.innerText = ""; 
+        checkTie = 0
     }
 }
 
 const showWinner = (winner) =>{
-msg.innerText = `congrats winner is ${winner}`
+msg.innerHTML = `congrats winner is ${winner}`
 msgContainer.classList.remove("hide")
 disableBtns()
 }
 
-
-const tie = (tie) =>{
+const matchTieShow = () =>{
+    tieMatch.classList.remove("hide")
 
 }
+
+
 
 const checkWinner = () =>{
 
@@ -78,9 +104,9 @@ const checkWinner = () =>{
 
         // );
 
-        const posValue1 =   btns[pattern[0]].innerText;
-        const posValue2 =   btns[pattern[1]].innerText;
-        const posValue3 =   btns[pattern[2]].innerText;
+        const posValue1 =   btns[pattern[0]].innerHTML;
+        const posValue2 =   btns[pattern[1]].innerHTML;
+        const posValue3 =   btns[pattern[2]].innerHTML;
 
         if(posValue1 != "" && posValue2 != "" && posValue3 != ""){
 
@@ -90,6 +116,7 @@ const checkWinner = () =>{
 
                 console.log("winner" , posValue1);
                 showWinner(posValue1);
+                matchTie = false
             }
             else{
                 console.log("tie")
@@ -103,4 +130,5 @@ const checkWinner = () =>{
 
 
 newGameBtn.addEventListener("click" , resetGame);
+newGameBtn2.addEventListener("click" , resetGame);
 resertBtn.addEventListener("click" , resetGame)
